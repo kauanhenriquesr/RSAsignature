9f9ade83576684b7b026330a0594e6499938577e
lkaubs <221017050@aluno.unb.br>
2025-06-18 15:19:15 -0300

    Commit do Miller Rabin

---
+++ src/prime_gen.py
@@ -0,0 +1,39 @@
+import random
+
+def miller_rabin(n, k):
+    d = n - 1
+    r = 0
+    while d % 2 == 0:
+        d //= 2
+        r += 1
+    
+    for _ in range(k):
+        a = random.randint(2, n - 2)
+        x = pow(a, n-1, n)
+        if x == 1 or x == n - 1:
+            continue
+        for _ in range(r - 1):
+            x = pow(x, 2, n)
+            if x == n - 1:
+                continue
+            if x == 1:
+                return False
+        return False
+    return True
+
+def gera_impar():
+    num = random.randint(2**1023, 2**1024 - 1)
+    
+    if num % 2 == 0:
+        num += 1
+    return num
+
+def gera_primo(iteracoes):    
+    tentativas = 0
+    while True:
+        tentativas += 1
+        candidato = gera_impar()
+        
+        if miller_rabin(candidato, iteracoes):
+            print(f"Primo encontrado após {tentativas} tentativas")
+            return candidato
---
+++ tests/test_prime_gen.py
@@ -0,0 +1,22 @@
+from src.prime_gen import miller_rabin, gera_impar, gera_primo
+import pytest
+
+def test_gera_impar():
+    assert gera_impar() % 2 == 1
+    assert gera_impar() % 2 != 0
+
+def test_miller_rabin():
+    assert miller_rabin(29, 40) == True
+    assert miller_rabin(31, 40) == True
+    assert miller_rabin(37, 40) == True
+    assert miller_rabin(41, 40) == True
+    assert miller_rabin(43, 40) == True
+    assert miller_rabin(47, 40) == True
+    assert miller_rabin(53, 40) == True
+    assert miller_rabin(59, 40) == True
+    assert miller_rabin(61, 40) == True
+    assert miller_rabin(67, 40) == True
+    assert miller_rabin(71, 40) == True
+
+def test_gera_primo():
+    assert miller_rabin(gera_primo(40), 40) == True
