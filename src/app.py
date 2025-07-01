98117dd1798a8e00b15cd128a7eb12cf35402b1c
Marcelo Vitor <marcelovitor890@gmail.com>
2025-07-01 10:04:35 -0300

    Correção função assina

--- src/app.py
+++ src/app.py
@@ -2,6 +2,7 @@ import base64
 from src.verifica_assina import assinatura, verifica
 from src.rsa import gera_chave
 
+
 def assina(file_path, n, private_key):
     assinaturaarq = assinatura(file_path, n, private_key)
     print("Assinatura gerada com sucesso!")
@@ -10,7 +11,9 @@ def assina(file_path, n, private_key):
     with open(file_path[0:-4]+"_assinatura.sig", "w") as f:
         f.write(assinaturaarq)
         print("-"*30)
-        print(f"Assinatura salva em {file_path[0:-4]+"_assinatura.sig"} com sucesso!")
+        print(
+            f"Assinatura salva em {file_path[0:-4]}_assinatura.sig com sucesso!")
+
 
 def verifica_assinatura(file_path, assinatura, n, public_key):
     print("-"*30)
@@ -23,14 +26,15 @@ def verifica_assinatura(file_path, assinatura, n, public_key):
     else:
         print("Assinatura INVÁLIDA ou não corresponde ao arquivo.")
 
+
 def main(args):
     if args == [] or args[0] in ["-h", "--help"]:
         print("Assinatura Digital")
-        print ("="*100)
-        print("Uso:\n" \
-        "   - python src/__main__.py -gen_keys\n" \
-        "   - python src/__main__.py -sign <arquivo>\n" \
-        "   - python src/__main__.py -verify <arquivo> <assinatura>\n")
+        print("="*100)
+        print("Uso:\n"
+              "   - python src/__main__.py -gen_keys\n"
+              "   - python src/__main__.py -sign <arquivo>\n"
+              "   - python src/__main__.py -verify <arquivo> <assinatura>\n")
         return 0
 
     elif args[0] == "-gen_keys":
@@ -60,15 +64,18 @@ def main(args):
             print("Uso: python src/__main__.py -sign <arquivo>")
             return 1
         file_path = args[1]
-        try: n = int(open("assets/n.key").read())
+        try:
+            n = int(open("assets/n.key").read())
         except FileNotFoundError:
-            print(f"Arquivo de N não encontrado. Por favor, gere as chaves primeiro.\n" \
-                  " - Se você não gerou as chaves, execute:\n" \
+            print(f"Arquivo de N não encontrado. Por favor, gere as chaves primeiro.\n"
+                  " - Se você não gerou as chaves, execute:\n"
                   "   python src/__main__.py -gen_keys")
             return 1
-        try: private_key = int(open("assets/private_key.key").read())
+        try:
+            private_key = int(open("assets/private_key.key").read())
         except FileNotFoundError:
-            print(f"Arquivo de chave privada não encontrado. Por favor, gere as chaves primeiro.")
+            print(
+                f"Arquivo de chave privada não encontrado. Por favor, gere as chaves primeiro.")
             return 1
         try:
             assina(file_path, n, private_key)
@@ -76,27 +83,32 @@ def main(args):
         except FileNotFoundError:
             print(f"Arquivo {file_path} não encontrado.")
             return 1
-    
+
     elif args[0] == "-verify":
         if len(args) != 3:
             print("Uso: python src/__main__.py -verify <arquivo> <assinatura>")
             return 1
         file_path = args[1]
         assinatura_file = args[2]
-        try: assinatura = open(assinatura_file).read()
+        try:
+            assinatura = open(assinatura_file).read()
         except FileNotFoundError:
-            print(f"Arquivo de assinatura {assinatura_file} não encontrado. Por favor, gere uma assinatura primeiro.")
+            print(
+                f"Arquivo de assinatura {assinatura_file} não encontrado. Por favor, gere uma assinatura primeiro.")
             return 1
 
-        try: n = int(open("assets/n.key").read())
+        try:
+            n = int(open("assets/n.key").read())
         except FileNotFoundError:
             print(f"Arquivo de N não encontrado. Por favor, gere as chaves primeiro.")
             return 1
 
-        try: public_key = int(open("assets/public_key.key").read())
+        try:
+            public_key = int(open("assets/public_key.key").read())
         except FileNotFoundError:
-            print(f"Arquivo de chave pública não encontrado. Por favor, gere as chaves primeiro.")
+            print(
+                f"Arquivo de chave pública não encontrado. Por favor, gere as chaves primeiro.")
             return 1
-        
+
         verifica_assinatura(file_path, assinatura, n, public_key)
         return 0
